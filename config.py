6a174425036f17b46f756ae3977fc7c5922e904e
aisyahnurms-02 <aisyahnurms@students.amikom.ac.id>
2026-07-21 23:04:59 +0700

    Update config.py to use hardcoded database settings

--- config.py
+++ config.py
@@ -1,19 +1,17 @@
-import os
-
 class Config:
     """
     Konfigurasi aplikasi Flask
     """
 
-    # Mengambil konfigurasi dari Vercel, jika tidak ada akan memakai "localhost" (untuk tes di komputer)
-    HOST = os.getenv("DB_HOST", "localhost")
-    PORT = int(os.getenv("DB_PORT", 3306))
-    USER = os.getenv("DB_USER", "root")
-    PASSWORD = os.getenv("DB_PASSWORD", "root")
-    DATABASE = os.getenv("DB_NAME", "db_akademik")
+    # Konfigurasi Database MySQL
+    HOST = "localhost"
+    PORT = 3306
+    USER = "root"
+    PASSWORD = ""
+    DATABASE = "db_akademik"
 
     # Secret Key Flask
-    SECRET_KEY = os.getenv("SECRET_KEY", "praktikum-flask-2026")
+    SECRET_KEY = "praktikum-flask-2026"
 
     # Konfigurasi Flask
-    DEBUG = os.getenv("FLASK_DEBUG", "True") == "True"
+    DEBUG = True
