ec120068fa52a2e2fee4f08c9599756bc1a7c27b
aisyahnurms-02 <aisyahnurms@students.amikom.ac.id>
2026-07-21 22:56:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,19 @@
+import os
+
 class Config:
     """
     Konfigurasi aplikasi Flask
     """
 
-    # Konfigurasi Database MySQL
-    HOST = "localhost"
-    PORT = 3306
-    USER = "root"
-    PASSWORD = "root"
-    DATABASE = "db_akademik"
+    # Mengambil konfigurasi dari Vercel, jika tidak ada akan memakai "localhost" (untuk tes di komputer)
+    HOST = os.getenv("DB_HOST", "localhost")
+    PORT = int(os.getenv("DB_PORT", 3306))
+    USER = os.getenv("DB_USER", "root")
+    PASSWORD = os.getenv("DB_PASSWORD", "root")
+    DATABASE = os.getenv("DB_NAME", "db_akademik")
 
     # Secret Key Flask
-    SECRET_KEY = "praktikum-flask-2026"
+    SECRET_KEY = os.getenv("SECRET_KEY", "praktikum-flask-2026")
 
     # Konfigurasi Flask
-    DEBUG = True
+    DEBUG = os.getenv("FLASK_DEBUG", "True") == "True"
